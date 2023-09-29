42966c8bea8951239cc0ce2230109a8bedcfd9d5
cdjellen <cdjellen@gmail.com>
2023-09-29 08:00:01 -0700

    format utilities

--- otb/utils/height_adjustment.py
+++ otb/utils/height_adjustment.py
@@ -3,11 +3,8 @@ from typing import Union
 import numpy as np
 
 
-def apply_fried_height_adjustment(
-    cn2: Union['pd.DataFrame', 'pd.Series', np.ndarray],
-    observed: float,
-    desired: float
-    ) -> Union['pd.DataFrame', 'pd.Series', np.ndarray]:
+def apply_fried_height_adjustment(cn2: Union['pd.DataFrame', 'pd.Series', np.ndarray], observed: float,
+                                  desired: float) -> Union['pd.DataFrame', 'pd.Series', np.ndarray]:
     """Height adjustment, see Chen 2019 Astron. Astrophys. 19 015.
 
     Args:
@@ -31,11 +28,10 @@ def apply_fried_height_adjustment(
 
 
 def apply_oermann_height_adjustment(
-    cn2: Union['pd.DataFrame', 'pd.Series', np.ndarray],
-    desired: float,
-    observed: float,
-    power_law_scaling: float = -4 / 3
-    ) -> Union['pd.DataFrame', 'pd.Series', np.ndarray]:
+        cn2: Union['pd.DataFrame', 'pd.Series', np.ndarray],
+        desired: float,
+        observed: float,
+        power_law_scaling: float = -4 / 3) -> Union['pd.DataFrame', 'pd.Series', np.ndarray]:
     """Height adjustment, see Oermann 2014, use -2/3 or -4/3 scaling laws.
     
     Args:
@@ -52,7 +48,7 @@ def apply_oermann_height_adjustment(
         assert desired > 0.0
     except AssertionError:
         raise ValueError('The observed and desired heights must be positive.')
-    
+
     cn2 *= (desired / observed)**(power_law_scaling)
-    
+
     return cn2
